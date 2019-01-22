9388ce2f3c1cd41a2d5f1951d99b0b9c4a4d74b5
nitin joshi <joshiinitin.joshi@gmail.com>
2019-01-22 20:24:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+for (var i=0; i< 64; i++){
+    document.getElementById("mainChessBoard").appendChild(document.createElement("div"))
+    .style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? '#7bb17b' : 'white';    
+}
